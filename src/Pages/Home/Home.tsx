535ca6a918639a956cb35a0b70cba67cf38d1cb9
Karston Kuciemba <kuciembakarston@gmail.com>
2024-11-14 15:54:21 -0600

    new scene added to home page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,20 @@
 import { Navbar } from "../../components";
 import { Box, Text } from "@mantine/core";
+import HomeScene from "../../scenes/HomeScene";
 
 export function Home() {
   return (
     <Box>
       <Navbar />
-      <Text>Home</Text>
+      <HomeScene />
+      <Box style={{ position: "relative", zIndex: 10, color: "#fff" }}>
+        <Text
+          component="h1"
+          style={{ fontSize: 30, fontWeight: 700, paddingTop: "400px" }}
+        >
+          This is testing
+        </Text>
+      </Box>
     </Box>
   );
 }

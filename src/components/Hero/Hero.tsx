666363ce8361b65ffa3d4981ba51777ae14c6084
Karston Kuciemba <kuciembakarston@gmail.com>
2024-11-14 16:36:02 -0600

    temp styling to hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Box, Text } from "@mantine/core";
+import "./useHeroStyles.css";
 
 export function Hero() {
   return (
-    <Box>
-      <Text>Hero</Text>
+    <Box className="hero-container">
+      <Box className="hero-content">
+        <Text component="p" className="greeting-text">
+          Hi, I am Karston <span className="waving-hand">👋</span>
+        </Text>
+        <Text className="subtitle-text">
+          Engineering software that powers progress
+        </Text>
+      </Box>
     </Box>
   );
 }

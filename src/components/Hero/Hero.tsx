2a0bdb35e34b890664e24876a39d52bee9d7b917
Karston Kuciemba <kuciembakarston@gmail.com>
2024-11-14 17:19:53 -0600

    fixed styling + added new render

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Box, Text } from "@mantine/core";
 import "./useHeroStyles.css";
+import { Canvas } from "@react-three/fiber";
+import { PerspectiveCamera } from "@react-three/drei";
+import Room from "./Room/Room";
 
 export function Hero() {
   return (
@@ -12,6 +15,12 @@ export function Hero() {
         <Text className="subtitle-text">
           Engineering software that powers progress
         </Text>
+      </Box>
+      <Box className="canvas-container">
+        <Canvas className="canvas">
+          <PerspectiveCamera makeDefault position={[0, 0, 10]} />
+          <Room />
+        </Canvas>
       </Box>
     </Box>
   );

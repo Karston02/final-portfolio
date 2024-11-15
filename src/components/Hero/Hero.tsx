07d0ae847be11d98f48dc7f030c11dce84a5f7ba
Karston Kuciemba <kuciembakarston@gmail.com>
2024-11-14 21:17:44 -0600

    hero continued

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import { Suspense } from "react";
+import { Loader } from "../../components";
 import { Box, Text } from "@mantine/core";
 import "./useHeroStyles.css";
 import { Canvas } from "@react-three/fiber";
-import { PerspectiveCamera } from "@react-three/drei";
+import { PerspectiveCamera, OrbitControls } from "@react-three/drei";
 import Room from "./Room/Room";
 
 export function Hero() {
@@ -18,8 +19,13 @@ export function Hero() {
       </Box>
       <Box className="canvas-container">
         <Canvas className="canvas">
-          <PerspectiveCamera makeDefault position={[0, 0, 10]} />
-          <Room />
+          <Suspense fallback={<Loader />}>
+            <PerspectiveCamera makeDefault position={[0, 10, 10]} />
+            <Room scale={0.8} position={[0, 0, 0]} rotation={[0, 270, 0]} />
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+            <OrbitControls enableZoom={true} />
+          </Suspense>
         </Canvas>
       </Box>
     </Box>

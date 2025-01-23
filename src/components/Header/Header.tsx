b36446a53b5fa25db850c1ed4f1c71f6014848f8
Karston Kuciemba <kuciembakarston@gmail.com>
2025-01-22 22:20:41 -0600

    header section progress 90% done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,44 @@
-import { Box, Text } from "@mantine/core";
+import { Box, Text, Grid, Button, Image } from "@mantine/core";
 import "./useHeaderStyles.css";
-import "../../assets/header-background.svg";
 
 export function Header() {
   return (
     <Box className="header-container">
-      <Box>
-        <Text>hello</Text>
-      </Box>
+      <Grid className="header-grid">
+        {/* Left Column for Text Content */}
+        <Grid.Col md={6} sm={12} className="header-content">
+          <Text className="header-title">
+            Hi, I'm <span className="highlight">Karston Kuciemba</span> <br />{" "}
+            <span className="highlight">a </span>Software Engineer
+          </Text>
+          <Text className="header-description">
+            I am a passionate software engineer building innovative solutions to
+            real-world problems, with experience in full-stack development,
+            algorithm visualization, and web design.
+          </Text>
+          <Button
+            className="header-btn"
+            variant="outline"
+            size="md"
+            component="a"
+            href="google.com"
+          >
+            Resume
+          </Button>
+        </Grid.Col>
+        {/* Right Column for Image */}
+        <Grid.Col
+          md={4}
+          sm={12} // Update for larger screens
+          className="header-image"
+        >
+          <Image
+            src="https://placehold.co/600x400"
+            alt="Hero Illustration"
+            className="hero-image"
+          />
+        </Grid.Col>
+      </Grid>
     </Box>
   );
 }

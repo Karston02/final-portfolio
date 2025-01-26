74d29894197129dfeb60eb8176daf7ef2d28ab7c
Karston Kuciemba <kuciembakarston@gmail.com>
2025-01-26 14:23:16 -0600

    fixed background color issues affecting buttons

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -4,11 +4,12 @@ import "./useHomeStyles.css";
 
 export function Home() {
   return (
-    <Box>
+    <Box className="page-wrapper">
       <Navbar />
       <Header />
       <Title className="section-title">Projects</Title>
       <Carousel />
+      <Title className="section-title">Experience</Title>
     </Box>
   );
 }

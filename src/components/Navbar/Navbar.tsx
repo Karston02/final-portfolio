747d7e910d3310e4117d02428421c57fb53c493d
Karston Kuciemba <kuciembakarston@gmail.com>
2025-01-26 11:43:53 -0600

    fixed navbar from Mantine v6 downgrade + added styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,5 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import {
-  Box,
-  Container,
-  Group,
-  Menu,
-  UnstyledButton,
-  Text,
-} from "@mantine/core";
+import { Box, Container, Group, UnstyledButton, Text } from "@mantine/core";
 import { useDisclosure, useMediaQuery } from "@mantine/hooks";
 import {
   IconHome,
@@ -17,40 +9,53 @@ import {
 } from "@tabler/icons-react";
 import "./useNavbarStyles.css";
 
-// TODO: Change so it goes to certain spot of the page rather than new route
 export function Navbar() {
   const [opened, { toggle }] = useDisclosure(false);
-  const navigate = useNavigate();
-
-  // detect screen size change for mobile vs desktop
   const isMobile = useMediaQuery("(max-width: 991px)");
 
   useEffect(() => {
-    if (!isMobile) {
-      if (opened) toggle(); // close the burger menu when switching to desktop view regardless
-    }
+    if (!isMobile && opened) toggle();
   }, [isMobile, opened, toggle]);
+
+  const scrollToSection = (id: any) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
 
   return (
     <Box className="header">
       <Container className="container">
-        <Box className="logo" onClick={() => navigate("/")}>
+        <Box className="logo" onClick={() => scrollToSection("home")}>
           <IconHome size={50} />
           <Text className="icon-text">KK</Text>
         </Box>
 
         {/* Desktop Links */}
         <Group className="links">
-          <UnstyledButton className="nav-link" component="a" href="/">
+          <UnstyledButton
+            className="nav-link"
+            onClick={() => scrollToSection("home")}
+          >
             Home
           </UnstyledButton>
-          <UnstyledButton className="nav-link" component="a" href="about">
+          <UnstyledButton
+            className="nav-link"
+            onClick={() => scrollToSection("about")}
+          >
             About
           </UnstyledButton>
-          <UnstyledButton className="nav-link" component="a" href="projects">
+          <UnstyledButton
+            className="nav-link"
+            onClick={() => scrollToSection("projects")}
+          >
             Projects
           </UnstyledButton>
-          <UnstyledButton className="nav-link" component="a" href="contact">
+          <UnstyledButton
+            className="nav-link"
+            onClick={() => scrollToSection("contact")}
+          >
             Contact
           </UnstyledButton>
         </Group>
@@ -62,35 +67,49 @@ export function Navbar() {
             <span></span>
             <span></span>
           </div>
-          <Menu opened={opened} onClose={toggle} position="bottom-end">
-            <Menu.Dropdown className="mobile-menu">
-              <Menu.Item component="a" className="mobile-menu-item" href="/">
-                <IconHome size={18} /> Home
-              </Menu.Item>
-              <Menu.Item
-                component="a"
-                className="mobile-menu-item"
-                href="about"
-              >
-                <IconUser size={18} /> About
-              </Menu.Item>
-              <Menu.Item
-                component="a"
-                className="mobile-menu-item"
-                href="projects"
-              >
-                <IconBriefcase size={18} /> Projects
-              </Menu.Item>
-              <Menu.Item
-                component="a"
-                className="mobile-menu-item"
-                href="contact"
-              >
-                <IconMail size={18} /> Contact
-              </Menu.Item>
-            </Menu.Dropdown>
-          </Menu>
         </div>
+
+        {/* Mobile Links Dropdown */}
+        {opened && (
+          <Box className="mobile-menu">
+            <UnstyledButton
+              className="mobile-link"
+              onClick={() => {
+                scrollToSection("home");
+                toggle();
+              }}
+            >
+              <IconHome size={18} /> Home
+            </UnstyledButton>
+            <UnstyledButton
+              className="mobile-link"
+              onClick={() => {
+                scrollToSection("about");
+                toggle();
+              }}
+            >
+              <IconUser size={18} /> About
+            </UnstyledButton>
+            <UnstyledButton
+              className="mobile-link"
+              onClick={() => {
+                scrollToSection("projects");
+                toggle();
+              }}
+            >
+              <IconBriefcase size={18} /> Projects
+            </UnstyledButton>
+            <UnstyledButton
+              className="mobile-link"
+              onClick={() => {
+                scrollToSection("contact");
+                toggle();
+              }}
+            >
+              <IconMail size={18} /> Contact
+            </UnstyledButton>
+          </Box>
+        )}
       </Container>
     </Box>
   );

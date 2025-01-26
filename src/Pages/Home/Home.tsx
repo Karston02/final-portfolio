27053a145d94f05a824cd87251c26939db2a29e4
Karston Kuciemba <kuciembakarston@gmail.com>
2025-01-26 13:39:26 -0600

    section title added

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import { Navbar, Header, Carousel } from "../../components";
-import { Box } from "@mantine/core";
+import { Box, Title } from "@mantine/core";
+import "./useHomeStyles.css";
 
 export function Home() {
   return (
     <Box>
       <Navbar />
       <Header />
+      <Title className="section-title">Projects</Title>
       <Carousel />
     </Box>
   );
